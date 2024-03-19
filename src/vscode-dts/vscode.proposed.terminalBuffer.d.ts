3d190f18abce672cc8ea9debc9de14117fc71b1e
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-03-19 06:42:14 -0700

    Iterating on terminal buffer api

--- src/vscode-dts/vscode.proposed.terminalBuffer.d.ts
+++ src/vscode-dts/vscode.proposed.terminalBuffer.d.ts
@@ -4,17 +4,50 @@
  *--------------------------------------------------------------------------------------------*/
 
 declare module 'vscode' {
+
+	// https://github.com/microsoft/vscode/issues/207504
+	//
+	// The consequences of exposing this API will be that we must have a synchronous copy of the
+	// terminal available on the extension host. This could be done in a couple of ways:
+	//
+	// - Send over diffs or serialized sections of the terminal periodically, this is probably
+	//   harder to accomplish than it's worth.
+	// - Run a parallel version of xterm.js on the extension host.
+	//
+	// The latter is already happening on both the renderer process and the pty host, so it's not a
+	// huge deal, especially since there is a headless version of xterm.js and its memory layout is
+	// quite compact; ~12 bytes per cell, so a fairly typical 160x20 viewport with 1000 scrollback
+	// would use:
+	//
+	// 160*20*12 + 160*1000*12 bytes = 38400 + 1920000 bytes = ~1.87mb
+	//
+	// Similar to on the pty host this could have limited scrollback if we wanted to restrict memory
+	// usage further, though that might be unexpected by extension authors and pose a challenge if
+	// they want to read the entire output of a command.
+	//
+	// One of the niceties of doing this is that the extension host is close to the pty host, so
+	// there is low latency in sending updates to the extension host, even on remote connections by
+	// sending data between these processes pty host -> server -> ext host. If all data events are
+	// in order then xterm.js will guarantee that they remain in sync.
+
 	export interface Terminal {
-		readonly buffers: TerminalBuffers;
+		readonly buffers: TerminalBufferSet;
 	}
 
-	export interface TerminalBuffers {
+	export interface TerminalBufferSet {
+		/**
+		 * The active buffer of the terminal. This is the buffer that is currently being displayed.
+		 */
 		readonly active: TerminalBuffer;
+
 		readonly normal: TerminalBuffer;
 		readonly alternate: TerminalBuffer;
 	}
 
 	export interface TerminalBuffer {
+		/**
+		 * The type of this buffer.
+		 */
 		readonly type: TerminalBufferType;
 
 		/**
@@ -29,12 +62,47 @@ declare module 'vscode' {
 
 		// TODO: Can we omit cursor x, y, base y, viewport y?
 
-		// NOTE: Throws when line isn't valid or is trimmed
+		/**
+		 * Returns a text line denoted by the line number. Note that the returned object is *not*
+		 * live and changes to the buffer are not reflected.
+		 *
+		 * TODO: This could be in range [0, trimmedLineCount + length (exclusive)] if we just want
+		 *       to pass the empty string for empty lines.
+		 * @param line A line number in [trimmedLineCount, trimmedLineCount + length (exclusive)].
+		 *
+		 * @throws When the line number is not valid and/or the line has been trimmed from the
+		 * buffer.
+		 */
 		lineAt(line: number): TerminalBufferLine;
+
+		/**
+		 * Get the text of this buffer. A substring can be retrieved by providing a range. The range
+		 * will be {@link TerminalBuffer.validateRange adjusted}.
+		 *
+		 * @param range The range to get the text for. If not provided, the entire buffer's text
+		 * will be returned.
+		 */
+		getText(range?: TerminalBufferRange): string;
+
+		/**
+		 * Ensure a range is completely contained in this terminal.
+		 *
+		 * @param range A range.
+		 * @returns The given range or a new, adjusted range.
+		 */
+		validateRange(range: TerminalBufferRange): Range;
 	}
 
 	export enum TerminalBufferType {
+		/**
+		 * The normal buffer of a terminal. This is the buffer that is active when the terminal
+		 * is first created and features scrollback.
+		 */
 		Normal,
+		/**
+		 * The alternate buffer of a terminal. This buffer is explicitly requested by the
+		 * application running in the terminal and does not feature scrollback.
+		 */
 		Alternate
 	}
 
@@ -79,6 +147,9 @@ declare module 'vscode' {
 	}
 
 	export namespace window {
+		/**
+		 * Fires when {@link Terminal} {@link TerminalBufferSet.active} changes.
+		 */
 		export const onDidChangeTerminalBuffer: Event<TerminalBufferChangeEvent>;
 	}
 
