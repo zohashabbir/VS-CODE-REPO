1aa8e0a4cf0bbc4ba6c5769e58eadc5ac5d51562
Parasaran <v.parasaran@gmail.com>
2024-10-10 00:14:54 +0530

    229280: Added a terminal feature setting to allow smart paste

--- src/vs/workbench/contrib/terminal/common/terminal.ts
+++ src/vs/workbench/contrib/terminal/common/terminal.ts
@@ -165,6 +165,7 @@ export interface ITerminalConfiguration {
 	scrollback: number;
 	commandsToSkipShell: string[];
 	allowChords: boolean;
+	allowSmartPaste: boolean;
 	allowMnemonics: boolean;
 	cwd: string;
 	confirmOnExit: ConfirmOnExit;
