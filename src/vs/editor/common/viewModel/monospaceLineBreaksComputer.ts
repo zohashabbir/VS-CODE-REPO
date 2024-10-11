302a3e9f0add0202be7ec5bda3ad1ddcbbe2d78d
Griffon Langyer <146424306+rfon6ngy@users.noreply.github.com>
2024-10-11 16:33:03 +0200

    Fix Hygiene

--- src/vs/editor/common/viewModel/monospaceLineBreaksComputer.ts
+++ src/vs/editor/common/viewModel/monospaceLineBreaksComputer.ts
@@ -434,8 +434,8 @@ function createLineBreaks(classifier: WrappingCharacterClassifier, _lineText: st
 
 		visibleColumn += charWidth;
 		// literal \n shall trigger a softwrap
-		if (lineText.charAt(i-3) != '\\' && lineText.charAt(i-2) == '\\' && lineText.charAt(i-1) == 'n') {
-			visibleColumn += breakingColumn
+		if (lineText.charAt(i-3) !== '\\' && lineText.charAt(i-2) === '\\' && lineText.charAt(i-1) === 'n') {
+			visibleColumn += breakingColumn;
 		}
 		// check if adding character at `i` will go over the breaking column
 		if (visibleColumn > breakingColumn) {
